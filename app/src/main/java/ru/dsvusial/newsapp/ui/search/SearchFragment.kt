1355ebfce035004ffb06b73bddb88aaeea0ad97c
Oleg <olegkostinds@gmail.com>
2022-08-03 14:19:43 +0300

    bottom navigation

---
+++ app/src/main/java/ru/dsvusial/newsapp/ui/search/SearchFragment.kt
@@ -0,0 +1,24 @@
+package ru.dsvusial.newsapp.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.dsvusial.newsapp.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() =  _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+}
